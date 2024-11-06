fe41ceb6b2fc2b83cc6bee498b728f0defef4caa
hossain <hossainahamedk@gmail.com>
2024-11-07 01:00:09 +0600

    fixed some issue in first part

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
 import TodoCard from './components/TodoCard'
 
 function App() {
 
   return (
-    <>
-      <div className='bg-gray-300 w-72 h-screen font-semibold'>
+    <div className='flex gap-4 w-[2000px] overflow-x-scroll'>
+
+      <div className='overflow-y-scroll bg-gray-300 w-80 font-semibold h-screen scrollbar-thin scrollbar-thumb-blue-800 scrollbar-track-slate-300'>
         <div className='flex justify-between p-4'>
           <h1>Incomplete</h1>
           <p className='bg-gray-400 px-2 rounded-md'>0</p>
@@ -22,7 +22,98 @@ function App() {
           <TodoCard />
         </div>
       </div>
-    </>
+
+      <div className='overflow-y-scroll bg-gray-300 w-80 font-semibold h-screen scrollbar-thin scrollbar-thumb-blue-800 scrollbar-track-slate-300'>
+        <div className='flex justify-between p-4'>
+          <h1>To Do</h1>
+          <p className='bg-gray-400 px-2 rounded-md'>0</p>
+        </div>
+
+        <div>
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
+      </div>
+
+      <div className='overflow-y-scroll bg-gray-300 w-80 font-semibold h-screen scrollbar-thin scrollbar-thumb-blue-800 scrollbar-track-slate-300'>
+        <div className='flex justify-between p-4'>
+          <h1>Doing</h1>
+          <p className='bg-gray-400 px-2 rounded-md'>0</p>
+        </div>
+
+        <div>
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
+      </div>
+
+      <div className='overflow-y-scroll bg-gray-300 w-80 font-semibold h-screen scrollbar-thin scrollbar-thumb-blue-800 scrollbar-track-slate-300'>
+        <div className='flex justify-between p-4'>
+          <h1>Under Review</h1>
+          <p className='bg-gray-400 px-2 rounded-md'>0</p>
+        </div>
+
+        <div>
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
+      </div>
+
+      <div className='overflow-y-scroll bg-gray-300 w-80 font-semibold h-screen scrollbar-thin scrollbar-thumb-blue-800 scrollbar-track-slate-300'>
+        <div className='flex justify-between p-4'>
+          <h1>Completed</h1>
+          <p className='bg-gray-400 px-2 rounded-md'>0</p>
+        </div>
+
+        <div>
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
+      </div>
+
+      <div className='overflow-y-scroll bg-gray-300 w-80 font-semibold h-screen scrollbar-thin scrollbar-thumb-blue-800 scrollbar-track-slate-300'>
+        <div className='flex justify-between p-4'>
+          <h1>Overdue</h1>
+          <p className='bg-gray-400 px-2 rounded-md'>0</p>
+        </div>
+
+        <div>
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+          <TodoCard />
+        </div>
+      </div>
+
+    </div>
   )
 }
 
